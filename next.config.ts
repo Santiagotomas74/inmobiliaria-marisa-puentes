5dd43ce9b6d14260886293a21cc992e33d02cc70
Santiago Taher <chrismar747@gmail.com>
2026-09-12 13:32:29 -0300

    fix de ficha pdf v11

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["sharp"],
+  outputFileTracingIncludes: {
+    "/api/**/*": ["./node_modules/@img/sharp-linux-x64/**/*"],
+  },
 };
 
 export default nextConfig;
